596d75e6691f7ed80647a39f172a441c7082ca4d
vvhung <vanvthy@gmail.com>
2021-05-11 17:29:53 +0700

    push todo app to github

---
+++ src/dist/TodoList.dev.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+// export default function TodoList({todos, completeTodo, deleteTodo}) {
+// return (
+// <ul>
+// {
+// todos.map((tbl_hocsinh) => (
+// <li key={tbl_hocsinh._id}>
+// {+tbl_hocsinh.completed ? <strike>{tbl_hocsinh.name}</strike> : tbl_hocsinh.name}
+// <div className="btn-groups">
+// <button className="complete" onClick={() => completeTodo(tbl_hocsinh._id)}>{+tbl_hocsinh.completed ? 'Rework' : 'Complete'}</button>
+// <button className="delete" onClick={() => deleteTodo(tbl_hocsinh._id)}>Delete</button>
+// </div>
+// </li>
+// ))
+// }
+// </ul>
+// );
+// }
+"use strict";
